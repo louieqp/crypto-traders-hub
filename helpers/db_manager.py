06772150d2e9002a08ff221b2b6151893bb0e5b2
Luis Quinones <luis_quinones10@hotmail.com>
2022-11-03 09:08:37 -0400

    added userProfiles function, modified commands to properly use async/await

--- helpers/db_manager.py
+++ helpers/db_manager.py
@@ -6,7 +6,7 @@ async def get_user_from_trade_id(trade_id: int) -> int:
     """
     # Query for user_id linked to trade_id
     async with aiosqlite.connect("database/database.db") as db:
-        async with db.execute("SELECT user_id FROM trades WHERE id=? LIMIT 1", (trade_id)) as cursor:
+        async with db.execute("SELECT user_id FROM trades WHERE id=? LIMIT 1", (trade_id,)) as cursor:
             rows = await cursor.fetchone()
             return rows[0] if rows is not None else -1
 
@@ -18,9 +18,22 @@ async def get_leaderboard() -> list:
 
 async def getUserProfile(user_id: int):
     async with aiosqlite.connect("database/database.db") as db:
-        async with db.execute("SELECT user_id, wins, losses, total_profit, avg_profit FROM user_profiles") as cursor:
+        async with db.execute("SELECT user_id, wins, losses, total_profit, avg_profit FROM user_profiles WHERE user_id=?", (user_id,)) as cursor:
             result = await cursor.fetchone()
             return result
+
+async def getUserTrades(user_id: int) -> list:
+    """
+    Description.
+
+    :param user_id: 
+    """
+    async with aiosqlite.connect("database/database.db") as db:
+        result = await db.execute(
+            "SELECT trade_id, coin, type, profit, close_price FROM user_trades WHERE user_id=? ORDER BY trade_id", (user_id,))
+        async with result as cursor:
+            rows = await cursor.fetchall()
+            return rows
 
 async def open_trade(user_id: int, trade_type: str, coin: str, open_price: float, target: float, stoploss: float, leverage: int = 10, vip: bool = False) -> int:
     """
@@ -35,7 +48,7 @@ async def open_trade(user_id: int, trade_type: str, coin: str, open_price: float
         async with result as cursor:
             rows = await cursor.fetchone()
             trade_id = rows[0]+1 if rows is not None else 1
-            await db.execute("INSERT INTO trades(id, user_id, type, coin, open, target, stoploss, leverage, vip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            await db.execute("INSERT INTO trades(id, user_id, type, coin, open_price, target, stoploss, leverage, vip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                         (trade_id, user_id, trade_type, coin, open_price, target, stoploss, leverage, vip))
             await db.commit()
             return trade_id
@@ -61,7 +74,7 @@ async def set_initial_close_trade(trade_id: int, open_price: float) -> None:
 async def get_trade_left_to_close(trade_id: int) -> int:
     # Get left to close trade
     async with aiosqlite.connect("database/database.db") as db:
-        result = await db.execute("SELECT left_to_close FROM trades_progress WHERE trade_id=?", (trade_id))
+        result = await db.execute("SELECT left_to_close FROM trades_progress WHERE trade_id=?", (trade_id,))
         async with result as cursor:
             rows = await cursor.fetchone()
             left_to_close = rows[0]
@@ -84,7 +97,7 @@ async def close_trade_percent(trade_id: int, closed_price: float, closed_percent
             await db.execute("INSERT INTO closing_points(id, trade_id, closed_price, closed_percent, closed_at) VALUES (?, ?, ?, ?, CURRENT_TIMESTAMP)", (close_id, trade_id, closed_price, closed_percent))
             await db.commit()
         
-        result = await db.execute("SELECT left_to_close FROM trades_progress WHERE trade_id=?", (trade_id))
+        result = await db.execute("SELECT left_to_close FROM trades_progress WHERE trade_id=?", (trade_id,))
         async with result as cursor:
             rows = await cursor.fetchone()
             left_to_close = rows[0]
@@ -101,8 +114,8 @@ async def close_trade(trade_id: int, close: float) -> list:
     async with aiosqlite.connect("database/database.db") as db:
         await db.execute("UPDATE trades SET close_price=?, closed_at=CURRENT_TIMESTAMP WHERE id=?", (close, trade_id))
         await db.commit()
-        result = db.execute(
-            "SELECT id, type, coin, open_price, target, stoploss, vip FROM trades WHERE id=?", (trade_id))
+        result = await db.execute(
+            "SELECT id, type, coin, open_price, target, stoploss, vip FROM trades WHERE id=?", (trade_id,))
         async with result as cursor:
             rows = await cursor.fetchone()
             return rows
@@ -121,12 +134,12 @@ async def add_trade(user_id: int, trade_type: str, coin: str, open_price: float,
         async with result as cursor:
             rows = await cursor.fetchone()
             trade_id = rows[0]+1 if rows is not None else 1
-            await db.execute("INSERT INTO trades(id, user_id, type, coin, open, close, target, stoploss, leverage, vip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            await db.execute("INSERT INTO trades(id, user_id, type, coin, open_price, close_price, target, stoploss, leverage, vip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
                         (trade_id, user_id, trade_type, coin, open_price, close, target, stoploss, leverage, vip))
             await db.commit()
 
         user_trades = await db.execute(
-            "SELECT id, type, coin, open, target, stoploss, vip FROM trades WHERE user_id=? AND close IS NULL", (user_id))
+            "SELECT id, type, coin, open_price, target, stoploss, vip FROM trades WHERE user_id=? AND close IS NULL", (user_id,))
         async with user_trades as cursor:
             rows = await user_trades.fetchall()
             return rows
@@ -139,42 +152,10 @@ async def remove_trade(user_id: int, trade_id: int) -> list:
     :param reason: The reason why the user should be warned.
     """
     async with aiosqlite.connect("database/database.db") as db:
-        await db.execute("DELETE FROM trades WHERE id=?", (trade_id))
+        await db.execute("DELETE FROM trades WHERE id=?", (trade_id,))
         await db.commit()
         result = await db.execute(
-            "SELECT id, type, coin, open_price, target, stoploss, vip  FROM trades WHERE user_id=? AND close IS NULL", (user_id))
+            "SELECT id, type, coin, open_price, target, stoploss, vip  FROM trades WHERE user_id=? AND close IS NULL", (user_id,))
         async with result as rows:
             open_trades = await rows.fetchall()
             return open_trades
-
-async def get_user_trades(user_id: int, status: str) -> list:
-    """
-    Description.
-
-    :param user_id: 
-    """
-    async with aiosqlite.connect("database/database.db") as db:
-        cursor = db.cursor()
-        return 
-
-async def get_closed_trades() -> list:
-    """
-    Description.
-
-    :param user_id: 
-    """
-    async with aiosqlite.connect("database/database.db") as db:
-        cursor = db.cursor()
-
-        return 
-
-async def get_all_trades() -> list:
-    """
-    Description.
-
-    :param user_id: 
-    """
-    async with aiosqlite.connect("database/database.db") as db:
-        cursor = db.cursor()
-
-        return 
