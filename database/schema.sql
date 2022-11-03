06ab3d06346ee5b99e38b45cbdc4ed1afbcc17b4
Luis Quinones <luis_quinones10@hotmail.com>
2022-11-03 09:07:13 -0400

    added user_trades view and fixed user_profile view

--- database/schema.sql
+++ database/schema.sql
@@ -41,10 +41,10 @@ CREATE TABLE IF NOT EXISTS `closing_points` (
 CREATE VIEW IF NOT EXISTS `trades_progress` AS
   SELECT 
     cp.trade_id AS `trade_id`, 
-    t.open_price, 
-    t.close_price, -- NULL close indicates that the trade is still open, otherwise we have the price at which the trade was totally closed
+    t.open_price AS `open_price`, 
+    t.close_price AS `close_price`, -- NULL close indicates that the trade is still open, otherwise we have the price at which the trade was totally closed
     (100-SUM(cp.closed_percent)) AS `left_to_close`,
-    ((SUM(cp.closed_percent/100 * cp.closed_price)-t.open_price)/t.open_price) AS `profit`
+    (CASE WHEN SUM(cp.closed_percent) = 0 THEN 0 ELSE ((SUM(cp.closed_percent/100 * cp.closed_price)-t.open_price)/t.open_price) * 100 * t.leverage END) AS `profit`
   FROM trades t JOIN closing_points cp ON t.id = cp.trade_id
   GROUP BY cp.trade_id
 ;
@@ -54,20 +54,31 @@ CREATE VIEW IF NOT EXISTS `leaderboard` AS
     t.user_id AS `user_id`,
     t.trade_id AS `trade_id`,
     t.coin AS `coin`,
-    tp.profit AS `profit`
+    tp.profit * t.leverage AS `profit`
   FROM trades t JOIN trades_progress tp ON t.id = tp.trade_id
-  GROUP BY tp.trade_id
   ORDER BY tp.profit DESC LIMIT 10
 ;
 
 CREATE VIEW IF NOT EXISTS `user_profiles` AS
   SELECT 
     t.user_id AS `user_id`,
-    COUNT(CASE WHEN profit > 0 THEN 1 ELSE 0 END) AS `wins`,
-    COUNT(CASE WHEN profit <= 0 THEN 1 ELSE 0 END) AS `losses`,
+    COUNT(CASE WHEN profit > 0 THEN 1 END) AS `wins`,
+    COUNT(CASE WHEN profit <= 0 THEN 1 END) AS `losses`,
+    COUNT(*) AS `total`,
     SUM(profit) AS `total_profit`,
-    AVG(CASE WHEN profit > 0 THEN profit ELSE 0 END) AS `avg_profit`
+    AVG(CASE WHEN profit > 0 THEN profit END) AS `avg_profit`
   FROM trades t JOIN trades_progress tp ON t.id = tp.trade_id
   WHERE t.close_price IS NOT NULL
   GROUP BY t.user_id
+;
+
+CREATE VIEW IF NOT EXISTS `user_trades` AS
+  SELECT 
+    t.user_id AS `user_id`,
+    t.id AS `trade_id`,
+    t.coin AS `coin`,
+    t.type AS `type`,
+    t.close_price AS `close_price`,
+    tp.profit AS `profit`
+  FROM trades t JOIN trades_progress tp on t.id=tp.trade_id
 ;
